8860c161b44319a8ee0d75cc9eb18264f678aec1
Juan Camilo Londoño <camilondonoc@gmail.com>
2024-09-07 18:36:51 -0500

    Condicional Años Bisiestos
    Código Años Bisiestos

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -1,7 +1,7 @@
 
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
+import static ejercicios.AnnosBisiestos.evaluar;
 import java.util.Scanner;
 
 /**
@@ -11,7 +11,11 @@ import java.util.Scanner;
 public class AnnosBisiestos {
     public static String evaluar(int anno){
         // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
+        if ((anno % 4 == 0 && anno % 100 != 0)||(anno % 400 == 0)){
+            return anno + " es bisiesto";
+        }else {
+            return anno + " no es bisiesto";
+        }   
     }
     
     public static void main(String[] args) {
